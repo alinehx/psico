eed3a908e703f5d4515ea1d3d72ecf42cdee9310
Marcel Tavares <marcel.tavares@keyrus.com.br>
2016-11-09 10:43:45 -0200

    globalized

--- angular/app/scripts/services/globalized.js
+++ angular/app/scripts/services/globalized.js
@@ -1,8 +1,8 @@
 'use strict';
 angular.module('psico2App')
   .factory('globalized', function($window) {
-    var actualHost = 'http://localhost:1337';
-    //var actualHost = 'http://35.163.118.191:1337';
+    //var actualHost = 'http://localhost:1337';
+    var actualHost = 'http://35.163.118.191:1337';
 
     return actualHost;
   });
